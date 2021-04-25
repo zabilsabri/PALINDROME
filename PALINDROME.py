8b982e8c1dc3c9929f60238d7807baf3ce3113a2
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-04-26 06:03:15 +0800

    Add files via upload

---
+++ PALINDROME.py
@@ -0,0 +1,14 @@
+__foundby__ = "zabilsabri"
+
+
+kata = str(input("MASUKKA KATA: "))
+
+for x in(kata):
+    pass
+for i in reversed(kata):
+    pass
+
+if x == i:
+    print("True")
+else:
+    print("false")
